2052d6fee4916b901ab1f4d957d2fb11544ab142
edbert.uniskai@gmail.com <edbert.uniskai@gmail.com>
2024-03-22 09:43:18 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -19,8 +19,8 @@ class Form1(Form1Template):
 
   def convert_click(self, **event_args):
     """This method is called when the button is clicked"""
-    convert_result = anvil.server.call('convert', self.input_text)
-    print(convert_result)
+    convert_result = anvil.server.call('convert',  self.text_area.text)
+    
     if convert_result:
       self.result.visible = True
       self.result.text = convert_result
