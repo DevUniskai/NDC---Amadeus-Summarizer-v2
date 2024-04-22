8f771e5329a917fe67b88dbecee00beaeb1f8581
edbert.uniskai@gmail.com <edbert.uniskai@gmail.com>
2024-04-22 03:29:57 +0000

    Edited new_file.js

--- theme/assets/new_file.js
+++ theme/assets/new_file.js
@@ -1,3 +1,14 @@
-function cpy(text) {
-	navigator.clipboard.writeText(text)
-}  
+// function cpy(text) {
+// 	navigator.clipboard.writeText(text)
+// }  
+ function cpy(text) {
+
+  const newElement = document.createElement("textarea");
+  newElement.value = text;
+  console.log(text)
+  // newElement.style.visibility = "hidden";
+  document.body.appendChild(newElement);
+  newElement.select();
+  document.execCommand("copy");
+  document.body.removeChild(newElement);
+  }
